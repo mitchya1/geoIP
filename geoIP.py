56ff4dbf3c5794abc2cf98f15d9a09a36a4b1f39
Mitch <mitchell.anderson@colorado.edu>
2018-07-27 10:36:13 -0600

    cleaned up, stopped using readline()

--- geoIP.py
+++ geoIP.py
@@ -2,32 +2,31 @@
 from geoip import geolite2 as gl2
 import datetime as dt
 import mysql.connector
-TODAY = dt.datetime.today().strftime("%m-%d")
-ip = open("/opt/geoIP/errors.log", "r")
-addr = ip.readlines()
 geodb = mysql.connector.connect (
   host = "localhost",
   user = "<user>",
-  password = "<password>",
-  database = "<database name>"
+  password = "<pw>",
+  database = "<db>"
 )
 dbcursor = geodb.cursor()
-
-#delete yesterday's logs
+#clear old
 sql = "delete from log where id;"
 dbcursor.execute(sql)
 geodb.commit()
 
-for line in addr:
-  line = line.rstrip('\n')
-  match = gl2.lookup(line)
-  if match is not None:
-    origin = match.country
-    sql = "insert into log (country, ip) values (%s, %s)"
-    val = (origin, line)
-    dbcursor.execute(sql, val)
-    geodb.commit()
-  else:
-    print("")
-    break
+with open ('/opt/geoIP/errors.log') as ip:
+  for line in ip:
+    lines = line.rstrip('\n')
+    match = gl2.lookup(lines)
+    if match is not None:
+      origin = match.country
+      sql = "insert into log (country, ip) values (%s, %s)"
+      val = (origin, line)
+      dbcursor.execute(sql, val)
+      geodb.commit()
+      #good to have for troubleshooting
+      #print(origin, "", lines)
+    else:
+      ip.close()
+      break
 ip.close()
